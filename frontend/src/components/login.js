7c58ffcc39a331fc919381da66b56a4cce7bf5ef
Shreyamtl <shreyamittal0811@gmail.com>
2025-09-05 20:44:08 +0530

    add-ons in Loginpage

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -11,12 +11,16 @@ function Login() {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-cover bg-center"
+    <div className="flex justify-center items-center h-screen bg-cover bg-center "
   style={{ backgroundImage: "url(https://images.unsplash.com/photo-1605379399642-870262d3d051?q=80&w=1206&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)" }}
     >
-    <div className="bg-white/80 shadow-2xl rounded-2xl p-8 w-96 backdrop-blur-sm">
-        <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
-          Login to CODRITHM
+    <div className="bg-white/90 shadow-2xl rounded-3xl p-12 w-[500px] backdrop-blur-sm border-2 border-blue-500"
+    style={{
+    boxShadow: "0 0 20px 5px rgba(59, 130, 246, 0.5)" // blue glow
+    }}>
+        <h1 className="text-2xl font-bold text-center text-indigo-800 mb-8 typing-animation">WELCOME TO CODERITHM</h1>
+        <h2 className="text-3xl font-bold text-gray-800 mb-6 ">
+          Login with 
         </h2>
         <form onSubmit={handleSubmit} className="space-y-5">
           <div>
@@ -25,7 +29,7 @@ function Login() {
             </label>
             <input
               type="text"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-400 focus:outline-none"
+              className="w-full px-5 py-3 border border-blue-400 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none"
               placeholder="Enter username"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
@@ -39,7 +43,7 @@ function Login() {
             </label>
             <input
               type="password"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-400 focus:outline-none"
+              className="w-full px-4 py-2 border border-blue-300 rounded-lg focus:ring-2 focus:ring-indigo-400 focus:outline-none"
               placeholder="Enter password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
@@ -51,7 +55,7 @@ function Login() {
             type="submit"
             className="w-full bg-indigo-600 text-white font-semibold py-2 rounded-lg shadow-md hover:bg-indigo-700 transition duration-300"
           >
-            Login
+            Continue
           </button>
         </form>
 
